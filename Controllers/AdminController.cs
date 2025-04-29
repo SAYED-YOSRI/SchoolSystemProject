a09f6abd33fa54f163b9b4a063688b1659175a20
pc <pc@DESKTOP-SJ3LJPK>
2025-04-29 03:23:43 +0200

    "create showAllStudents Action that && create function AddStudent" #

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using final_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace final_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult ShowAllStudents()
+        {
+            var students = context.Students
+            .Include(s => s.Enrollments)
+            .ThenInclude(e => e.Course)
+            .ToList();
+
+            return View("ShowAllStudents", students);
+        }
+
+        public IActionResult createStudent ()
+        {
+            return View("createStudent");
+        }
+
+        public IActionResult saveCreate(Student student) 
+        {
+            if (student != null)
+            {
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("ShowAllStudents");
+        
+        }
+    }
+}
