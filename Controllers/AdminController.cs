dc2fc111292604afad097f25a591001ffec15568
pc <pc@DESKTOP-SJ3LJPK>
2025-04-30 02:59:26 +0200

    uodate function create - add function create to teacher - update reepresentation of show all students

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,20 +1,28 @@
 ﻿using final_project.Models;
+using final_project.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace final_project.Controllers
 {
     public class AdminController : Controller
     {
+
         ApplicationDbContext context = new ApplicationDbContext();
         public IActionResult ShowAllStudents()
         {
-            var students = context.Students
+            StudentTeacherListsVM viewModel = new StudentTeacherListsVM()
+            {
+                Students = context.Students
             .Include(s => s.Enrollments)
             .ThenInclude(e => e.Course)
-            .ToList();
+            .ToList(),
 
-            return View("ShowAllStudents", students);
+                Teachers = context.Teachers.ToList()
+            };
+
+            return View("ShowAllStudents", viewModel);
         }
 
         public IActionResult createStudent ()
@@ -26,12 +34,83 @@ namespace final_project.Controllers
         {
             if (student != null)
             {
-                context.Students.Add(student);
-                context.SaveChanges();
+                var exist = context.Students.Any(s=> s.Email == student.Email);
+
+                if (exist) 
+                {
+                    ModelState.AddModelError("Email", "This Email is Already Exist");
+
+                    return View("createStudent", student);
+                }
+                else 
+                {
+                    context.Students.Add(student);
+                    context.SaveChanges();
+                }
+                
             }
 
             return RedirectToAction("ShowAllStudents");
         
         }
+
+        public IActionResult creatTeacher()
+        {
+            var courses = context.Courses
+        .Select(c => new SelectListItem
+        {
+            Value = c.CourseId.ToString(),
+            Text = c.Title
+        }).ToList();
+
+            ViewBag.Courses = courses;
+
+            return View("creatTeacher");
+        }
+
+        [HttpPost]
+        public IActionResult saveCreateTeacher(Teacher teacher, List<int> SelectedCourseIds)
+        {
+            if (teacher != null)
+            {
+                var exists = context.Teachers.Any(t => t.Email == teacher.Email);
+
+                if (exists)
+                {
+                    ModelState.AddModelError("Email", "This Email is Already Exist");
+
+                    ViewBag.Courses = context.Courses
+                        .Select(c => new SelectListItem
+                        {
+                            Value = c.CourseId.ToString(),
+                            Text = c.Title
+                        }).ToList();
+
+                    return View("creatTeacher", teacher);
+                }
+
+                
+                if (SelectedCourseIds != null && SelectedCourseIds.Any())
+                {
+                    var selectedCourses = context.Courses
+                        .Where(c => SelectedCourseIds.Contains(c.CourseId))
+                        .ToList();
+
+                    teacher.Courses = selectedCourses;
+                }
+
+                context.Teachers.Add(teacher);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("ShowAllStudents");
+        }
+
+
+
+
+
+
+
     }
 }
